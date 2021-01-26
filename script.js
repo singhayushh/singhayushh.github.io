9c149aa9a2537e6a82388cd09d248cf89f6d668d
singhayushh <coding.ayush@gmail.com>
2021-01-26 14:28:52 +0530

    scrolling bug fixes

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+/* 
+    Most of the code here is taken from stackoverflow.
+    Credits to all the individual authors of the snippets I used.
+*/
+
 jQuery(document).ready(function($) {
+
+    // Disable scrolling when mouse is over nav
+    $( 'nav' ).on( 'mousewheel DOMMouseScroll', function ( e ) {
+        var e0 = e.originalEvent,
+            delta = e0.wheelDelta || -e0.detail;
+    
+        this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
+        e.preventDefault();
+    });
+
+    // Smooth scrolling
     $(function() {
         $('a[href*=#]:not([href=#])').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
                 if (target.length) {
-                    $('.container').animate({
+                    $('html, body').animate({
                     scrollTop: target.offset().top
                     }, 1000);
                     return false;
@@ -14,3 +30,4 @@ jQuery(document).ready(function($) {
         });
     });
 });
+
